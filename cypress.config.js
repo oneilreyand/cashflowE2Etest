821551d65254265e35e7fdbcc1e8f4f081e4d336
oneilreyand <114917910+oneilreyand@users.noreply.github.com>
2024-11-17 11:07:48 +0700

    add-config-cypressCoverage-istambul

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,26 @@
 const { defineConfig } = require("cypress");
+const codeCoverage = require('@cypress/code-coverage/task');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // require("@cypress/code-coverage/task")(on, config); // Aktifkan code coverage
+      codeCoverage(on, config);
+      return config;
+    },
+    baseUrl: getBaseUrlByEnv(process.env.CYPRESS_ENV || "local"), // Ambil dari environment variable atau default
+    env: {
+      environment: process.env.CYPRESS_ENV || "local", // Default ke 'local' jika tidak ditentukan
     },
   },
 });
+
+function getBaseUrlByEnv(environment) {
+  const envUrls = {
+    local: "http://localhost:5173",
+    dev: "cashflow.assist.id",
+    prod: "cashflow.assist.id",
+  };
+
+  return envUrls[environment] || envUrls.local;
+}
